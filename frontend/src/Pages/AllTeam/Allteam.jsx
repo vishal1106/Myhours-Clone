d62232fc3dce9ebbe9f1267f2b706bcacdb20c0b
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2023-01-29 11:26:25 +0530

    project responsive

--- frontend/src/Pages/AllTeam/Allteam.jsx
+++ frontend/src/Pages/AllTeam/Allteam.jsx
@@ -44,7 +44,7 @@ const Allteam = () => {
         getData();
       }, []);
 
-      function randomIntFromInterval(min, max) { // min and max included 
+      function randomIntFromInterval(min, max) { 
         return Math.floor(Math.random() * (max - min + 1) + min)
       }
 
